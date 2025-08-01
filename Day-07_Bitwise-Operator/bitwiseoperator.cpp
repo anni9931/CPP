d4beff950b86fcc54ee720ec89a1ab7f87f3fe1e
anni9931 <freelancing.anni@gmail.com>
2025-08-01 14:07:23 +0530

    Initial commit

--- Day-07_Bitwise-Operator/bitwiseoperator.cpp
+++ Day-07_Bitwise-Operator/bitwiseoperator.cpp
@@ -16,4 +16,5 @@ int main()
     cout << "Left shift of b by 2:" << (b << 2) << endl;
     cout << "Right shift of a by 2:" << (a >> 2) << endl;
     cout << "Right shift of b by 2:" << (b >> 2) << endl;
+    cout << "Right shift of b by 2:" << (b >> 2) << endl;
 }
